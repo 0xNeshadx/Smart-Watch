c45cae51b852d6211a53a9649bad637391722fa2
0xNeshadx <neshadimagify@gmail.com>
2025-03-05 19:38:00 +0600

    added some code

--- js/script.js
+++ js/script.js
@@ -19,3 +19,17 @@ for (let i = 0; i < ringButtons.length; i++) {
     productImage.src = productImageBase + color + ".png";
   });
 }
+
+
+function selectWristSize(size) {
+    const sizes = ["S", "M", "L", "XL"];
+    for (let i = 0; i < sizes.length; i++) {
+      const button = document.getElementById("size-" + sizes[i]);
+      const elemnt = sizes[i];
+      if (size === elemnt) {
+        button.classList.add("border-purple-600");
+      } else {
+        button.classList.remove("border-purple-600");
+      }
+    }
+  }
